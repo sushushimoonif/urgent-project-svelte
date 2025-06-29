76e31c9f78e4818f0c7dd49d64c703dde6f0b0ae
sushushimoonif <64478993+sushushimoonif@users.noreply.github.com>
2025-06-29 17:00:59 +0800

    Updated RealTimeCalc.svelte

--- src/components/RealTimeCalc.svelte
+++ src/components/RealTimeCalc.svelte
@@ -733,7 +733,7 @@
                     <!-- 刻度数字 -->
                     <text x="-1" y="9" fill="white" fill-opacity="0.7" font-size="12">120</text>
                     <text x="-1" y="70" fill="white" fill-opacity="0.7" font-size="12">100</text>
-                    <text x="4" y="132" fill="white" fill-opacity="0.7" font-size="12">80</text>
+                    <text x="2" y="132" fill="white" fill-opacity="0.7" font-size="12">80</text>
                     <text x="4" y="194" fill="white" fill-opacity="0.7" font-size="12">60</text>
                     <text x="14" y="259" fill="white" fill-opacity="0.7" font-size="12">40</text>
                     <text x="9" y="321" fill="white" fill-opacity="0.7" font-size="12">20</text>
