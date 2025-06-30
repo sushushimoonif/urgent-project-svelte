f4ee1269dfb877ff2d0b28ac23d0a99f0fa325bd
sushushimoonif <64478993+sushushimoonif@users.noreply.github.com>
2025-06-30 23:33:19 +0800

    Updated SwitcherGroup.svelte

---
+++ src/components/SwitcherGroup.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+
+    let { selectedData = $bindable()} = $props();
+</script>
+
+<div class="flex flex-col gap-4 justify-center items-center">
+    <Switcher left="作战" right="训练" bind:selectedData={selectedData[0]} />
+    <Switcher left="地面" right="空中" bind:selectedData={selectedData[1]} />
+</div>
+
